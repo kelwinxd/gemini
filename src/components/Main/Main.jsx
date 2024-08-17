126709d46cefcfaeb10e80e7f818e76b3440fdfa
Kelwin Esechiel <90807426+kelwinxd@users.noreply.github.com>
2024-08-17 20:17:50 -0300

    ui finish

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -38,6 +38,18 @@ const Main = () => {
               <img src={assets.code_icon} alt="" />
             </div>
           </div>
+
+          <div className="main-bottom">
+            <div className="search-box">
+              <input type="text" name="" id="" placeholder='Type your Prompt' />
+              <div className='imgs-search'>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                <img src={assets.send_icon} alt="" />
+              </div>
+            </div>
+            <p>O Gemini pode apresentar informações imprecisas, inclusive sobre pessoas. Por isso, cheque as respostas. Sua privacidade e os apps do Gemini</p>
+          </div>
         </div>
     </div>
     </>
